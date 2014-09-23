5a6af17b4424885338f2e20687ce844368f73c13
Benny Liu <bennyliu@augmentum.com.cn>
2014-09-23 10:32:04 +0800

    add firupload.py

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 ideploy
 =======
-
 ## Usage
-### ci.yml
+### PreBuild tools
+#### idepoly.py
+>用于build前 替换一些文件和字段
+
+##### 应用场景
+* 多渠道build
+* 不同包显示不同的icon或者splash
+	
+##### 配置文件 ci.yml
 	replace_from_pics:
 	需要替换的文件路径集合  
 	replace_to_pics:
@@ -13,37 +20,29 @@ ideploy
 	template_target_files:
 	需要被替换的文本文件路径集合 
 	定义一个replace_content,例如 replace_content: Hello Yaml
+##### 运行
+	python idepoly.py (默认加载ci.yml)
+	python idepoly.py xx.yml (自定义的yml,格式参考ci.yml)
 	
+### AfterBuild tools
+#### firupload.py
+>用于打包完后上传到 [fir](http://fir.im>)
+
+##### 应用场景
+	打包好的x.ipa 或者 xx.apk 需要给QA进行测试，可以通过fir进行分发
+##### Install
+	 sudo easy_install clint
+	 sudo easy_install requests
+	 sudo easy_install requests_toolbelt
+	 
+##### config.yml
+	appid: 应用id (android: applicationId, ios:bundleId)
+	token: fir 提供的 token
+	filepath: xx.ipa/xx.apk 的路径
+	type: ios/android
 	
-## Example
-### ci.yml
-	replace_from_pics:
-  	- from/a.txt
-
-	replace_to_pics:
-  	- to/a.txt
+##### 运行
+	python firupload.py
 
 
-	template_source_files:
-  	- template/template-a.txt
-
-	template_target_files:
-  	- template/a.txt
-
-	id: helloworld
-	xx: Helloworld xx
-### In: template/template-a.txt
-	id1
-	{{id}}
-	id2
-	{{id}}
-	xx
-	{{xx}}
-	
-### Out: template/a.txt
-	id1
-	helloworld
-	id2
-	helloworld
-	xx
-	helloworld xx
+blog: [beiliubei](http://www.cnblogs.com/beiliubei/)
--- afterbuild/firupload.py
+++ afterbuild/firupload.py
@@ -1,12 +1,43 @@
+# -*- coding: utf-8 -*-
+
 __author__ = 'benny'
 import yaml
+import requests
+import json
+from requests_toolbelt import MultipartEncoder, MultipartEncoderMonitor
+from clint.textui.progress import Bar as ProgressBar
 
-infourl = 'http://fir.im/api/v2/app/info/%s?token=%s'
+infourl = 'http://fir.im/api/v2/app/info/%s?token=%s&type=%s'
 uploadurl = 'http://up.qiniu.com/'
 
+
+def my_callback(encoder):
+    encoder_len = len(encoder)
+    bar = ProgressBar(expected_size=encoder_len, filled_char='=')
+
+    def callback(monitor):
+        bar.show(monitor.bytes_read)
+
+    return callback
 
 if __name__ == '__main__':
     f = open('config.yml')
     x = yaml.load(f)
-    infourl = infourl % (x['appid'], x['token'])
-    print infourl
+    infourl = infourl % (x['appid'], x['token'], x['type'])
+    print '===== do get info from ', infourl, ' ====='
+    response = requests.get(infourl)
+    print json.loads(response.text)
+    pkgkey = json.loads(response.text)['bundle']['pkg']['key']
+    pkgtoken = json.loads(response.text)['bundle']['pkg']['token']
+    print pkgkey
+    print pkgtoken
+
+    print '===== do upload file',x['filepath'], 'to', uploadurl, ' ====='
+    e = MultipartEncoder(
+    fields={'key': pkgkey, 'token': pkgtoken,
+            'file': ('filename', open(x['filepath'], 'rb'), 'text/plain')}
+    )
+    callback = my_callback(e)
+    m = MultipartEncoderMonitor(e, callback)
+    uploadresponse = requests.post(uploadurl, data=m, headers={'Content-Type': m.content_type})
+    print uploadresponse.text
